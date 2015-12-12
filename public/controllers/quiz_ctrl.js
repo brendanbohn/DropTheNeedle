f31014d6de8a01f429d66f9a11ac9b1abf35fce9
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-12-11 16:34:16 -0800

    queue of playlist is playing but not properly, skipping some songs, NEED TO SQUISH THE BUG

--- public/controllers/quiz_ctrl.js
+++ public/controllers/quiz_ctrl.js
@@ -1,16 +1,57 @@
 angular.module('myApp.controllers')
 
-.controller('QuizController', [ '$scope', '$http', '$window', 'Auth', 'API', 'PlaylistId','$rootScope', function ($scope, $http, $window, Auth, API, PlaylistId, $rootScope) {
+.controller('QuizController', [ '$scope', '$http', '$window', 'Auth', 'API', 'PlaylistId','$rootScope', 'Playback', 'PlayQueue', 
+	function ($scope, $http, $window, Auth, API, PlaylistId, $rootScope, Playback, PlayQueue) {
 	
+	//gets vallues from SearchController(search bar form)
 	var user_id = PlaylistId.getOwnerId();
 	var playlist = PlaylistId.getPlaylistId();
 
-	console.log('user_id', user_id);
-	console.log('playlist', playlist);
-
+	//gets the tracks for playlist clicked on search page
 	API.getPlaylistTracks(user_id, playlist).then(function (tracks) {
+		
 		$scope.tracks = tracks.items;
-		console.log($scope.tracks);
+		setTrackList($scope.tracks);
+
+		$scope.toQueue = [];
+		$scope.tracklist.forEach( function(object){
+			$scope.toQueue.push(object.track.uri);
+		});
+
+		PlayQueue._queue = $scope.toQueue;
+		Playback.startPlaying(PlayQueue._queue[0]);
 	});	
+
+	$rootScope.$on('endtrack', function() {
+		PlayQueue.next();
+	});
+	$rootScope.$on('playqueuechanged', function() {
+		console.log("QUEUE:", PlayQueue._queue);
+		var position = PlayQueue.getPosition();
+		console.log(position);
+		Playback.startPlaying(PlayQueue._queue[position]);
+	});
+
+	//sets quiz length to a max of 20 songs. 
+	function setTrackList (array) {
+		$scope.tracklist = [];
+
+		if(array.length <= 20){
+			$scope.tracklist.concat(array);
+		} else if(array.length > 20) {
+			for(var i = 0; i < 20; i++) {
+				var j =	getRandomInt(0, array.length);
+				var spliced = array.splice(j, 1);
+				$scope.tracklist.push(spliced[0]); 
+			}
+		}
+	}
+	//gets random int for setting track list
+	function getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min)) + min;
+	}
+
 }]);
+
+
 

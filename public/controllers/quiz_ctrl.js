521734ebb376ef3f6bf1e13d48d65a4069dc1963
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-12-14 11:33:06 -0800

    PLAYLIST PLAYING IN ORDER WITHOUT SKIPPING, reseting audiotag every time so that events stop accumulating

--- public/controllers/quiz_ctrl.js
+++ public/controllers/quiz_ctrl.js
@@ -6,30 +6,39 @@ angular.module('myApp.controllers')
 	//gets vallues from SearchController(search bar form)
 	var user_id = PlaylistId.getOwnerId();
 	var playlist = PlaylistId.getPlaylistId();
-	var promise_array = [];
+	var promiseArray = [];
 	var position = 0;
+	var playlistTracks = [];
+	var shortenedPlaylist = [];
+	var shortenedPlaylistURIs = [];
 
 	//gets the tracks for playlist clicked on search page
-	API.getPlaylistTracks(user_id, playlist).then(function (tracks) {
+	API.getPlaylistTracks(user_id, playlist).then(function (data) {
 		
-		$scope.tracks = tracks.items;
-		setTrackList($scope.tracks);
-
-		$scope.toQueue = [];
-		$scope.tracklist.forEach( function(object){
-			var trackid = object.track.uri.split(':')[2];
-			$scope.toQueue.push(trackid);
+		playlistTracks = data.items;
+		setTrackList(playlistTracks);
+		
+		shortenedPlaylist.forEach( function(object){
+			var trackURI = object.track.uri.split(':')[2];
+			shortenedPlaylistURIs.push(trackURI);
 		});
-		for(var i = 0; i < $scope.toQueue.length; i++) {
-			promise_array.push(
-				API.getTrack($scope.toQueue[i])
-			);
+		for(var i = 0; i < shortenedPlaylistURIs.length; i++) {
+			promiseArray.push(API.getTrack(shortenedPlaylistURIs[i]));
 		}
-		$q.all(promise_array)
+		$q.all(promiseArray)
 		.then( function () {
-			console.log('PROMISE ARRAY', promise_array);
-			console.log('example url', promise_array[0].$$state.value.preview_url);
-			createAndPlayAudio(promise_array[0].$$state.value.preview_url);
+			//this loop removes any null preview_url's from the promise_array queue
+			for(var i = 0; i < promiseArray.length; i++){
+				if(promiseArray[i].$$state.value.preview_url === null) {
+					promiseArray.splice(i, 1);
+					console.log("SPLICED");
+				}
+			}
+			console.log('promise_array.length', promiseArray.length);
+			console.log('PROMISE ARRAY', promiseArray);
+			console.log('example url', promiseArray[0].$$state.value.preview_url);
+			createAndPlayAudio(promiseArray[0].$$state.value.preview_url);
+			
 		});
 
 	});	
@@ -40,49 +49,53 @@ angular.module('myApp.controllers')
 	var _progress = 0;
 	var _duration = 0;
 	var _trackdata = null;	
-	var audiotag = new Audio();
 
-	function createAndPlayAudio(url, callback, endcallback) {
+	function createAndPlayAudio(url) {
+
 		console.log('createAndPlayAudio', url);
-		if (audiotag.src !== null) {
-			audiotag.pause();
-			audiotag.src = null;
+		if(audiotag) {
+			audiotag = null;
 		}
+		var audiotag = new Audio();
 		audiotag.src = url;
+
 		audiotag.addEventListener('loadedmetadata', function() {
 			console.log('audiotag loadedmetadata');
+			console.log('metadata audiotag', audiotag);
 			_duration = audiotag.duration * 1000.0;
 			audiotag.volume = _volume / 100.0;
 			audiotag.play();
-			// callback();
 		}, false);
 		audiotag.addEventListener('ended', function () {
 			console.log('audiotag ended');
+			console.log('ended audiotag', audiotag);
 			_playing = false;
 			_track = '';
-			
-			audiotag.removeEventListener('ended', playNextSong(), false);
+			playNextSong();
 			// disableTick();
-			// $rootScope.$emit('endtrack');
+			$rootScope.$emit('endtrack');
 		}, false);
 	}
-		function playNextSong () {
-			if(position < promise_array.length){
-				position = position + 1;
-				createAndPlayAudio(promise_array[position].$$state.value.preview_url);
-			}
+
+
+	function playNextSong () {
+		if(position < promiseArray.length-1){
+			position = position + 1;
+			createAndPlayAudio(promiseArray[position].$$state.value.preview_url);
+			console.log("position ", position);
 		}
+	}
+
 	//sets quiz length to a max of 20 songs. 
 	function setTrackList (array) {
-		$scope.tracklist = [];
 
 		if(array.length <= 20){
-			$scope.tracklist.concat(array);
+			shortenedPlaylist.concat(array);
 		} else if(array.length > 20) {
 			for(var i = 0; i < 20; i++) {
 				var j =	getRandomInt(0, array.length);
 				var spliced = array.splice(j, 1);
-				$scope.tracklist.push(spliced[0]); 
+				shortenedPlaylist.push(spliced[0]); 
 			}
 		}
 	}
